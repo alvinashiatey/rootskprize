599d101527d46e37155fdbbe4e032d9304a5863d
alvinashiatey <alvinashiatey@gmail.com>
2020-09-02 21:22:39 -0400

    initial setup for kuenyehia root

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: `js/index.js`,
+  },
+  devServer: {
+    contentBase: "./dist/",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      filename: "./index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.s?[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "cheap-module-source-map",
+};
